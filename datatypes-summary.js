bbdb69f49b9e69b1874502b75d5dfc589a215fe5
nidhi <nidhisingh191091@gmail.com>
2025-08-28 17:40:13 +0530

    Adding datatype-summary.js .

---
+++ datatypes-summary.js
@@ -0,0 +1,24 @@
+// Primitive Datatype
+/*
+7 types: (Call by Value) --> String, Number, Boolean, Null, Undefined, Symbol, (for uniqueness of any value), BigInt 
+*/
+
+// Non-Primitive Datatype or Reference Datatype
+/*
+Array, Objects, Functions
+*/
+
+// Javascript is a dynmacially-typed language.
+
+
+/*
+Symbol:
+
+const id = Symbol('123');  //its's return type is Symbol 
+
+//even if the 2 symbols have same value , but the memeory address for both is different
+*/
+const id = Symbol('123');
+const aId = Symbol('123');
+
+console.log(id === aId);
