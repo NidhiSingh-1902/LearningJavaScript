0e9c4cfd18f9c57580a40f3c89109264fa86a106
nidhi <nidhisingh191091@gmail.com>
2025-08-29 15:18:23 +0530

    Adding 05_strings.js

---
+++ 05_strings.js
@@ -0,0 +1,26 @@
+// console.log(`${}`)   --> String Interpolation
+
+// const  newStr = String("nidhi");
+const newStr1 = new String("singh");
+console.log(newStr1[0]);  //output is s
+
+/*
+String Method's
+
+length, toUpperCase, charAt, indexOf, 
+
+SubString: 
+const newString = variableName.substring(start, end);
+
+slice, trim, replace 
+
+--> slice : takes the negative value as well.
+
+--> includes : verify that particular string or char is available or not.
+
+--> split(), join()
+
+
+
+
+*/
