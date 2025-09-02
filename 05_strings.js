a35de803c25d5a9bad7340af54510fa313663696
nidhi <nidhisingh191091@gmail.com>
2025-09-02 15:56:37 +0530

    Adding additional points.

--- 05_strings.js
+++ 05_strings.js
@@ -1,8 +1,8 @@
 // console.log(`${}`)   --> String Interpolation
 
 // const  newStr = String("nidhi");
-const newStr1 = new String("singh");
-console.log(newStr1[0]);  //output is s
+// const newStr1 = new String("singh");
+// console.log(newStr1[0]);  //output is s
 
 /*
 String Method's
@@ -24,3 +24,41 @@ slice, trim, replace
 
 
 */
+
+
+// mdn
+
+// const result = "cat".charAt(0);
+// console.log(result);
+
+// const strPrim = "foo";
+// const strPrim2 = String(1);
+// const strPrim3 = String(true);
+// const strObj = new String(strPrim);
+
+
+// console.log(strPrim);
+// console.log(strPrim2);
+// console.log(strPrim3);
+// console.log(strObj);
+
+// console.log(typeof strPrim);
+// console.log(typeof strPrim2);
+// console.log(typeof strPrim3);
+// console.log(typeof strObj);
+
+// console.log("===========================");
+// const newStr = new String("nidhi");
+// console.log(newStr);
+// console.log(typeof newStr);
+
+
+
+// console.log("====================");
+// const s1 = "2+2";
+// console.log(s1);
+// console.log(eval(s1));
+
+
+const varName = "Anidhi";
+console.log(varName.charCodeAt(0));
